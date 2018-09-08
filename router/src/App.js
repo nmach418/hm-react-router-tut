d8ba16be8b84a8eb4de20142131e7b2f40132edb
Boku <Boku@NHs-MacBook-Pro.local>
2018-09-08 19:35:39 -0400

    Created basic Home Component. Built route in App.js. Created About.js and Contact.js files.

--- router/src/App.js
+++ router/src/App.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from "react-router-dom";
-
+import Home from "./components/Home";
 
 //Temp Dummy Component created as a stateless functional Component - returning some very basic JSX within the div.
-const NewRoute = () => {
-  return (
-  <div>
-    <p>New Route</p>
-  </div>
-  );
+// const NewRoute = () => {
+//   return (
+//   <div>
+//     <p>New Route</p>
+//   </div>
+//   );
 
-}
+// }
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
 
-        <Route path="/new" component={NewRoute} />
+        <Route path="/" component={Home} />
 
       </BrowserRouter>
 
---
+++ router/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// Note: the Home Component is in the form of a stateless functional component - as opposed to the class type components that we are more familiar with in class.
+
+const Home = () => {
+    return(
+        <div>
+            <p>Home</p>
+        </div>
+    );
+};
+
+export default Home;
