f82cbc4a8ea849d3afa29715b33ca9337e1e3531
Boku <Boku@NHs-MacBook-Pro.local>
2018-09-08 18:49:30 -0400

    Created route /new within BrowserRouter. Also created a stateless functional dummy  NewRoute dummy component and got it to render in the browser correctly when /new is entered in the url.

--- router/src/App.js
+++ router/src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
 
+
+//Temp Dummy Component created as a stateless functional Component - returning some very basic JSX within the div.
+const NewRoute = () => {
+  return (
+  <div>
+    <p>New Route</p>
+  </div>
+  );
+
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <BrowserRouter>
 
+        <Route path="/new" component={NewRoute} />
 
-      </div>
+      </BrowserRouter>
+
     );
   }
 }
