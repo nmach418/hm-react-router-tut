310f6c92562ec1d06ef500ea43059e4bb23728a5
Boku <Boku@NHs-MacBook-Pro.local>
2018-09-08 20:10:14 -0400

    Added keyword exact to the Home route in App.js to prevent Home from rendering on ALL pages.

--- router/src/App.js
+++ router/src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
     return (
       <BrowserRouter>
         <div>
-          <Route path="/" component={Home} />
+          <Route path="/" component={Home} exact />
           <Route path="/about" component={About} />
           <Route path="/contact" component={Contact} />
         </div>
